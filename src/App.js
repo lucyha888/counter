3cec1299fef197d703911511a61deafbb54280d1
lucyha <lucyha.0101@gmail.com>
2022-11-03 21:08:26 +0700

    Create Counter

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [color, setColor] = useState("black")
+  const [counter, setCounter] = useState(0)
+
+  useEffect(() => {
+    if (counter > 0) {
+      setColor("green")
+    }
+    if (counter < 0) {
+      setColor("red")
+    }
+    if (counter === 0) {
+      setColor("black")
+    }
+  }, [counter]) 
+  const handleIncrease = () => {
+    setCounter(counter + 1) 
+  }
+  const handleDecrease = () => {
+    setCounter(counter - 1)
+  }
+  const handleReset = () => {
+    setCounter(0)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div classNameName="App">
+       <div className="container">
+          <h1>Counter</h1>
+          <span id="value" style={{color:color}}>{counter}</span>
+       <div className="btn-group">
+          <button className="decrease" onClick={handleDecrease}>DECREASE</button>
+          <button className="reset" onClick={handleReset}>RESET</button>
+          <button className="increase" onClick={handleIncrease}>INCREASE</button>
+      </div>
     </div>
+  </div>
   );
 }
 
